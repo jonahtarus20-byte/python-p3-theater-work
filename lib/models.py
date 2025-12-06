f7a709569363314c4a851580e237c59d3050b72c
Your Name <youremail@example.com>
2025-12-06 23:19:22 +0300

    finished code challenge

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -7,4 +7,47 @@ convention = {
 }
 metadata = MetaData(naming_convention=convention)
 
+#class for models
 Base = declarative_base(metadata=metadata)
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String, nullable=False)
+
+    # One-to-many relationship
+    auditions = relationship("Audition", back_populates="role")
+
+    @property
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    @property
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_auditions = [a for a in self.auditions if a.hired]
+        return hired_auditions[0] if hired_auditions else "no actor has been hired for this role"
+
+    def understudy(self):
+        hired_auditions = [a for a in self.auditions if a.hired]
+        return hired_auditions[1] if len(hired_auditions) > 1 else "no actor has been hired for understudy for this role"
+
+
+class Audition(Base):
+    __tablename__ = "auditions"
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    phone = Column(Integer)
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    role = relationship("Role", back_populates="auditions")
+
+    def call_back(self):
+        self.hired = True
